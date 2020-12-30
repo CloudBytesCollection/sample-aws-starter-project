df7e9060d6cf6a943fd43c1d2c8695ba5c530d9a
Edy Rosado <edy.vision18@gmail.com>
2020-12-30 09:12:41 -0700

    Updated folder name

--- services/sample-service.js
+++ services/sample-service.js
@@ -4,6 +4,7 @@
 const handlerUtil = require('../libs/utils/handler-util');
 
 module.exports.getHelloMessage = () => {
+    // Placeholder for something awesome!
     return new Promise((resolve) => {
         resolve(handlerUtil.formatHandlerResponse(200, { response: 'Hello!' }));
     });
